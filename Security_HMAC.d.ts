3b486050f7e2eb9be827b5a9712e2fa9055cf812
lord-saumagen <lord.saumagen@gmail.com>
2016-11-14 02:50:15 +0100

    Added missing HMAC test.

--- Security_HMAC.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../TS_Base/TS_Base.d.ts" />
-/// <reference path="../TS_Security/TS_Security.d.ts" />
-/// <reference path="Scripts/qunit.d.ts" />
-declare namespace TS_Security_test {
-}
